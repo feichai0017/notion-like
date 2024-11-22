2da6bf3c1ab1342092a3a49dac9a2f6407f19831
eric_song <songguocheng348@gmail.com>
2024-11-22 19:37:40 +1100

    Add realtime update document title

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,15 +1,43 @@
 "use client";
 
+import {useQuery} from "convex/react";
+import {api} from "@/convex/_generated/api";
+import {useParams} from "next/navigation";
+import {Id} from "@/convex/_generated/dataModel";
+import {MenuIcon} from "lucide-react";
+import {Title} from "@/app/(main)/_components/title";
+
 interface NavbarProps {
     isCollapsed: boolean;
     onResetWidth: () => void;
 };
 
 
-export const Navbar = () => {
+export const Navbar = ( {isCollapsed, onResetWidth} : NavbarProps) => {
+    const params = useParams();
+    const document = useQuery(api.documents.getById, {documentId: params.documentId as Id<"documents">});
+
+
+    if (document === undefined) {
+        return <p>Loading...</p>
+    }
+    if (document === null) {
+        return null;
+    }
     return (
-        <div>
-            Navbar
-        </div>
+       <>
+        <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center gap-x-4">
+            {isCollapsed && (
+                <MenuIcon
+                    role="button"
+                    onClick={onResetWidth}
+                    className="h-6 w-6 text-muted-foreground"
+                />
+            )}
+            <div className="flex items-center justify-center wf-ull">
+                <Title initialData={document}/>
+            </div>
+        </nav>
+       </>
     );
 };
---
+++ app/(main)/_components/title.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import {Doc} from "@/convex/_generated/dataModel";
+import {api} from "@/convex/_generated/api";
+import {useMutation} from "convex/react";
+import {useRef, useState} from "react";
+import {Input} from "@/components/ui/input";
+import {Button} from "@/components/ui/button";
+
+interface TitleProps {
+    initialData: Doc<"documents">;
+};
+
+export const Title = ({initialData} : TitleProps) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+    const update = useMutation(api.documents.update);
+
+    const [title, setTitle] = useState(initialData.title || "Untitled");
+    const [isEditing, setIsEditing] = useState(false);
+
+    const enableInput = () => {
+        setTitle(initialData.title);
+        setIsEditing(true);
+        setTimeout(() => {
+            inputRef.current?.focus();
+            inputRef.current?.setSelectionRange(0, inputRef.current.value.length);
+        }, 0);
+    };
+
+    const disableInput = () => {
+        setIsEditing(false);
+    };
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setTitle(e.target.value);
+        update({
+            id: initialData._id,
+            title: e.target.value || "Untitled",
+        });
+    };
+
+    const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === "Enter") {
+            disableInput();
+        }
+    }
+    return (
+        <h1 className="flex items-center gap-x-1">
+            {!!initialData.icon && <p>{initialData.icon}</p>}
+            {isEditing? (
+                <Input
+                    ref={inputRef}
+                    onClick={enableInput}
+                    onBlur={disableInput}
+                    onChange={onChange}
+                    onKeyDown={onKeyDown}
+                    value={title}
+                    className="h-7 px-2 focus-visible:ring-transparent"
+                />
+            ) : (
+                <Button
+                    onClick={enableInput}
+                    variant="ghost"
+                    size="sm"
+                    className="font-normal h-auto p-1"
+                >
+                    <span className="truncate">
+                        {initialData?.title}
+                    </span>
+                </Button>
+            )}
+        </h1>
+    )
+}
