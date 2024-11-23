7f160393d6c3a5a44b316f9ee3c68e008db2e1f1
eric_song <songguocheng348@gmail.com>
2024-11-23 14:40:47 +1100

    Fix params type error

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,13 +1,23 @@
 import {Id} from "@/convex/_generated/dataModel";
 import {DocumentIdContent} from "@/app/(main)/(routes)/documents/[documentId]/document-id-content";
+import { Suspense, use} from "react";
 
 
-const DocumentIdPage = async ({params}: {
-    params: {
+interface PageProps {
+    params: Promise<{
         documentId: Id<"documents">;
-    }
-}) => {
-    return <DocumentIdContent documentId={params.documentId} />;
+    }>;
+}
+
+const DocumentIdPage = ({params}: PageProps) => {
+    const resolvedParams = use(params);
+
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <DocumentIdContent documentId={resolvedParams.documentId} />
+        </Suspense>
+    );
 };
+
 
 export default DocumentIdPage;
