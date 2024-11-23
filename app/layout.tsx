e046a740d3a40897fcc4ebfcaf3bdbb610b3c9ce
eric_song <songguocheng348@gmail.com>
2024-11-23 15:58:47 +1100

    Add analytic on vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import {ConvexClientProvider} from "@/components/providers/convex-provider";
 import {Toaster} from "sonner";
 import {ModalProvider} from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from "@/lib/edgestore";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -48,6 +49,7 @@ export default function RootLayout({
           >
             <Toaster position="bottom-center" />
             <ModalProvider/>
+            <Analytics/>
             {children}
           </ThemeProvider>
         </EdgeStoreProvider>
